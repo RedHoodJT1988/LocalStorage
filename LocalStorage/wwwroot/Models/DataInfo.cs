8f3a33603feb0bfec043f83d1a4905b264ec4967
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-12-05 11:10:01 -0800

    Add project files.

---
+++ LocalStorage/wwwroot/Models/DataInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalStorage.Models
+{
+    public class DataInfo
+    {
+        public string? Value { get; set; }
+        public int Length { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
